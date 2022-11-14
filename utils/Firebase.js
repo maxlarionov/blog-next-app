4cb6478ee1b5d04d5cc3dabcaca7a750bd5e986b
maxlarionov <maksym.larionov@gmail.com>
2022-11-14 17:20:34 +0200

    vercel fix

--- utils/Firebase.js
+++ utils/Firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app"
 import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
-	apiKey: process.env.NEXT_PUBLIC_API_KEY,
-	authDomain: process.env.NEXT_PUBLIC_API_AUTH_DOMAIN,
-	projectId: process.env.NEXT_PUBLIC_API_PROJECT_ID,
-	storageBucket: process.env.NEXT_PUBLIC_API_STORAGE_BUCKET,
-	messagingSenderId: process.env.NEXT_PUBLIC_API_APP_MSID,
-	appId: process.env.NEXT_PUBLIC_API_APP_ID,
-	measurementId: process.env.NEXT_PUBLIC_API_APP_MID
+	apiKey: `${process.env.NEXT_PUBLIC_API_KEY}`,
+	authDomain: `${process.env.NEXT_PUBLIC_API_AUTH_DOMAIN}`,
+	projectId: `${process.env.NEXT_PUBLIC_API_PROJECT_ID}`,
+	storageBucket: `${process.env.NEXT_PUBLIC_API_STORAGE_BUCKET}`,
+	messagingSenderId: `${process.env.NEXT_PUBLIC_API_APP_MSID}`,
+	appId: `${process.env.NEXT_PUBLIC_API_APP_ID}`,
+	measurementId: `${process.env.NEXT_PUBLIC_API_APP_MID}`
 };
 
 export const app = initializeApp(firebaseConfig)
